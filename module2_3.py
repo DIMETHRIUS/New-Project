42886eeb67ce61a49d07df2462afd7c3b6aac57b
DIMETHRIUS <103068865+DIMETHRIUS@users.noreply.github.com>
2024-11-16 14:16:10 +0300

    Add files via upload

--- module2_3.py
+++ module2_3.py
@@ -2,8 +2,9 @@ my_list: list[int] = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 positive = []
 index = 0
 while index < len(my_list):
-    if my_list [index] < 0:
+    if my_list[index] < 1:
         break
     positive.append(my_list[index])
     index += 1
+positive.append(99)
 print(positive)
