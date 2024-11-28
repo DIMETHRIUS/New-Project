d47d83574d7b273b5e06a1c55e612f8705d6668d
DIMETHRIUS <103068865+DIMETHRIUS@users.noreply.github.com>
2024-11-28 17:46:42 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,21 +1,26 @@
 
-def send_email(message,*, recipient ='urban.teacher@mail.com', sender = 'university.help@gmail.com' ):
+def send_email(message, recipient,*, sender = 'university.help@gmail.com' ):
+    if sender == recipient:
 
-    if(sender==recipient):
         print('Нельзя отправить письмо самому себе!')
 
-    elif(sender=='university.help@gmail.com'):
-        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    elif "@" not in recipient or not (
+                recipient.endswith(".com") or recipient.endswith(".ru") or recipient.endswith(".net")):
 
-    elif(sender=='an.nabogdanova@yandex.ru'):
-        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
-
-    elif('@','.com' not in recipient or '@', '.com' not in sender):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 
+    elif sender == "university.help@gmail.com":
 
-send_email('Нельзя отправить письмо самому себе!', recipient='university.help@gmail.com')
-send_email('Письмо успешно отправлено с адреса {sender>} на адрес {recipient}.')
-send_email('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.', sender='an.nabogdanova@yandex.ru')
-send_email('Невозможно отправить письмо с адреса {sender} на адрес {recipient}', sender='vasyok1337@gmail.com')
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+
+    else:
+
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+
+
+send_email('Нельзя отправить письмо самому себе!',sender='university.help@gmail.com', recipient='university.help@gmail.com')
+send_email('Невозможно отправить письмо с адреса {sender} на адрес {recipient}', recipient='urban.teacher@mail.by')
+send_email('Письмо успешно отправлено с адреса {sender} на адрес {recipient}.', recipient='urban.student@mail.ru')
+send_email('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.', sender='an.nabogdanova@yandex.ru', recipient= 'urban.student@mail.ru')
+
 
