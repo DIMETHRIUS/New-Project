6ce2c4178bc9eff7e9340d0af77a05231cf9f96e
DIMETHRIUS <103068865+DIMETHRIUS@users.noreply.github.com>
2024-12-13 18:16:01 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -1,14 +1,15 @@
 class House:
+    def go_to(self, new_floor):
+        for i in range(1, new_floor + 1):
+            if 1 <= new_floor <= self.number_of_floors:
+                print(i)
+            elif 1 <= new_floor:
+                print(f'Такого этажа не существует')
+                break
     def __init__(self, name, number_of_floors,):
         self.name = name
         self.number_of_floors = number_of_floors
-        def go_to(self, new_floor):
-            for i in range(0, new_floor + 1):
-                if 1 <= new_floor <= self.number_of_floors:
-                    print(i)
-                elif 0 == new_floor:
-                    print(f'Такого этажа не существует: {new_floor}')
-                    break
+
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
